ecba28b289615bd9a8c3dc1327807ae5b94a183c
Islom <islom.boynazarov@gmail.com>
2026-05-27 01:10:52 +0500

    feat: create project folder structure

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-console": "warn",
+      semi: ["error", "always"],
+      "no-warning-comments": [
+        "warn",
+        { terms: ["todo", "fixme", "xxx"], location: "anywhere" },
+      ],
+      "no-inline-comments": "warn",
+      quotes: ["error", "double"],
+      "prettier/prettier": "error",
+    },
+  },
+];
