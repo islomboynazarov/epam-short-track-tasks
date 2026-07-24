8ee6e910f682b4f7f527d5800a61d7c1432f49e5
islomboynazarov <islom.boynazarov@gmail.com>
2026-07-24 11:22:31 +0500

    feat: convert cardlist.tsx

--- class-components/src/components/CardList.tsx
+++ class-components/src/components/CardList.tsx
@@ -1,27 +1,51 @@
-import React from "react";
+// import React from "react";
+// import type { PokemonDetails } from "../types";
+// import Card from "./Card";
+
+// interface CardListProps {
+//   pokemons: PokemonDetails[];
+// }
+
+// class CardList extends React.Component<CardListProps> {
+//   render() {
+//     const { pokemons } = this.props;
+
+//     if (pokemons.length === 0) {
+//       return <p style={{ textAlign: "center" }}>No results found.</p>;
+//     }
+
+//     return (
+//       <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+//         {pokemons.map((pokemon) => (
+//           <Card key={pokemon.id} pokemon={pokemon} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// export default CardList;
+
 import type { PokemonDetails } from "../types";
 import Card from "./Card";
 
 interface CardListProps {
   pokemons: PokemonDetails[];
+  onCardClick: (id: number) => void;
 }
 
-class CardList extends React.Component<CardListProps> {
-  render() {
-    const { pokemons } = this.props;
-
-    if (pokemons.length === 0) {
-      return <p style={{ textAlign: "center" }}>No results found.</p>;
-    }
-
-    return (
-      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-        {pokemons.map((pokemon) => (
-          <Card key={pokemon.id} pokemon={pokemon} />
-        ))}
-      </div>
-    );
+function CardList({ pokemons, onCardClick }: CardListProps) {
+  if (pokemons.length === 0) {
+    return <p style={{ textAlign: "center" }}>No results found.</p>;
   }
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+      {pokemons.map((pokemon) => (
+        <Card key={pokemon.id} pokemon={pokemon} onClick={onCardClick} />
+      ))}
+    </div>
+  );
 }
 
 export default CardList;
