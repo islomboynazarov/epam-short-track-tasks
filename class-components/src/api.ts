bac5372a3331f931d599439c0ec14631ad0174a9
islomboynazarov <islom.boynazarov@gmail.com>
2026-07-24 22:05:52 +0500

    feat: upadate api.tsx

--- class-components/src/api.ts
+++ class-components/src/api.ts
@@ -1,4 +1,4 @@
-import type { PokemonDetails } from "./types";
+import type { PokemonDetails, PokemonDetailFull } from "./types";
 
 const BASE_URL = "https://pokeapi.co/api/v2";
 
@@ -20,6 +20,23 @@ export const fetchPokemonByName = async (name: string): Promise<PokemonDetails> 
     name: data.name,
     id: data.id,
     description: `Type: ${data.types.map((t: { type: { name: string } }) => t.type.name).join(", ")}. Base experience: ${data.base_experience}.`,
+    sprites: {
+      front_default: data.sprites.front_default,
+    },
+  };
+};
+
+export const fetchPokemonById = async (id: number): Promise<PokemonDetailFull> => {
+  const response = await fetch(`${BASE_URL}/pokemon/${id}`);
+  if (!response.ok) throw new Error(`Pokemon not found`);
+  const data = await response.json();
+  return {
+    name: data.name,
+    id: data.id,
+    height: data.height,
+    weight: data.weight,
+    types: data.types.map((t: { type: { name: string } }) => t.type.name),
+    abilities: data.abilities.map((a: { ability: { name: string } }) => a.ability.name),
     sprites: {
       front_default: data.sprites.front_default,
     },
